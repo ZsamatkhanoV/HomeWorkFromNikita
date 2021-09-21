dac023dd0f8cf76773b8b56abcf9097bfc354077
ZsamatkhanoV <zsamatkhanov@gmail.com>
2021-09-21 23:21:22 +0300

    feature/lessons added Main.java

---
+++ src/java/lesson_6/LandTransport.java
@@ -0,0 +1,12 @@
+package java.lesson_6;
+
+/*В классе Наземный добавляются поля:
+        -Количество колёс
+        - Расход топлива(л/100км)*/
+
+public abstract class LandTransport extends Transport {
+    int wheelsLight;
+    int wheelsHeavy;
+    int consumptionLight;
+    int consumptionHeavy;
+}
